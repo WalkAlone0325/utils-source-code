71151776238ba2e541716ddb3896fd75b3d110ac
独行 <jsw0325@163.com>
2021-03-03 09:36:50 +0800

    docs: update README.md file

--- articles/工具类函数/money.js
+++ articles/工具类函数/money.js
@@ -1,6 +1,7 @@
 /**
  * 将浮点数点左边的数每三位添加一个逗号，如 12000000.11 转化为『12,000,000.11』
  */
-function format (number) {
-  return number && number.replace(/(?!^)(?=(\d{3})+\.)/g, ',')
+function format(number) {
+  return number && number.String().replace(/(?!^)(?=(\d{3})+\.)/g, ',')
 }
+format(1223123546113)
